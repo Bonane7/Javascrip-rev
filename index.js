5e2df63137fc878e414b1a712d5a3929abf5b84f
Bonane Busoke <bonanebusoke@gmail.com>
2026-05-04 15:49:08 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,77 @@
+//1) le variable
+// const nom = "Alice";
+
+// let age = 20;
+// age = 30 ;
+
+// const ville = "paris";
+
+// console.log(nom, age, ville);
+
+//2)sting
+// let nom = "Alice";
+// let message = `salut ${nom}`;
+// console.log(message)
+
+//3 boolean
+// let isConnect = true;
+// let isAdmin = false;
+// console.log(typeof isConnect);
+
+//4 les object
+// let user = {nom : "Alice", age:30};
+
+// console.log(user.age)
+
+//5 cohersion
+// let strg = "5";
+// let nmb = 3;
+// let save = strg - nmb;
+// console.log(typeof save);
+// console.log(save)
+
+//6 comparaison
+
+// let age = 40;
+
+// if(age < 18){
+//     console.log("minuer")
+// }else if(age >= 18 && age <= 36){
+//     console.log('il est mageur')
+// }else{
+//     console.log("Vous avez depasser l'age de 36 ans")
+// }
+
+// let age = 10;
+// let status = age >= 18 ? "mageurs":"mineur";
+
+// console.log(status)
+
+// switch (jour = "lundi") {
+//     case "lundi":
+//     case "mardi": console.log("Début semaine"); break;
+//     case "vendredi": console.log("Presque weekend"); break;
+//     default: console.log("Milieu semaine");
+// }
+
+// switch (envions = "Kenya"){
+//          case "Kenya":
+//         break;
+//         case "Congo": console.log("congo airways");
+//         break;
+//         case "Rwanda": console.log("Rwandair");
+//        break;
+//         default:console.log("taks other transport please")
+// }
+
+const hotel="OUGANDA";
+
+switch(hotel ){
+    case "KENYA": console.log('KENYA HOTEL');
+    break;
+    case "RWANDA": console.log("SERENA HOTEL");
+    break;
+    case "BURUNDI": console.log("Burundi hotel")
+    break;
+    default:console.log("Hotel non fund")
+}
