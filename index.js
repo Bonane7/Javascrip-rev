57fbb94cbd82f883e9712eaaf9ca561d67babebf
Bonane Busoke <bonanebusoke@gmail.com>
2026-05-08 08:38:10 +0200

    js

--- index.js
+++ index.js
@@ -32,15 +32,15 @@
 
 //6 comparaison
 
-let age = 40;
+// let age = 40;
 
-if(age < 18){
-    console.log("minuer")
-}else if(age >= 18 && age <= 36){
-    console.log('il est mageur')
-}else{
-    console.log("Vous avez depasser l'age de 36 ans")
-}
+// if(age < 18){
+//     console.log("minuer")
+// }else if(age >= 18 && age <= 36){
+//     console.log('il est mageur')
+// }else{
+//     console.log("Vous avez depasser l'age de 36 ans")
+// }
 
 // let age = 10;
 // let status = age >= 18 ? "mageurs":"mineur";
@@ -54,24 +54,48 @@ if(age < 18){
 //     default: console.log("Milieu semaine");
 // }
 
-switch (envions = "Kenya"){
-         case "Kenya":
-        break;
-        case "Congo": console.log("congo airways");
-        break;
-        case "Rwanda": console.log("Rwandair");
-       break;
-        default:console.log("taks other transport please")
-}
+// switch (envions = "Kenya"){
+//          case "Kenya":
+//         break;
+//         case "Congo": console.log("congo airways");
+//         break;
+//         case "Rwanda": console.log("Rwandair");
+//        break;
+//         default:console.log("taks other transport please")
+// }
 
-const hotel="OUGANDA";
+// const hotel="OUGANDA";
 
-switch(hotel ){
-    case "KENYA": console.log('KENYA HOTEL');
-    break;
-    case "RWANDA": console.log("SERENA HOTEL");
-    break;
-    case "BURUNDI": console.log("Burundi hotel")
-    break;
-    default:console.log("Hotel non fund")
-}
+// switch(hotel ){
+//     case "KENYA": console.log('KENYA HOTEL');
+//     break;
+//     case "RWANDA": console.log("SERENA HOTEL");
+//     break;
+//     case "BURUNDI": console.log("Burundi hotel")
+//     break;
+//     default:console.log("Hotel non fund")
+// }
+
+// fonctions
+// function saluer(){
+//     console.log("Bonjoutr")
+// }
+
+// saluer();
+
+// function saluer (nom, postNom){
+//     console.log("Bonjour:"+ nom+ postNom)
+// }
+// saluer("Bonane","Busoke")
+
+// function addition(a , b){
+//     return a + b;
+// }
+// let resultat = addition(5, 3);
+
+// console.log(resultat)
+
+// const multiplier= function(a, b){
+// return a * b;
+// }
+// console.log(multiplier(5, 3))
